4d343353ec47e4665a802e1f68469c51df2d01f8
Oskar Schöldström <public@oxy.fi>
2015-11-07 16:23:10 -0500

    bring in latest changes

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 WP_CLI_HOST ?= vagrant
 DATABASE_EXPORT ?= database.sql
 
+DEV_HOST ?= example.dev
+PRODUCTION_HOST ?= example.com
+LOCAL_HOST ?= localhost:3000
+
+PRODUCTION_REMOTE_HOST ?= deploy@example.com:/home/www/example
+
 all:
 
 # Virtual Machine -------------------------------------------------------------
@@ -23,11 +29,45 @@ vm-clean:
 $(DATABASE_EXPORT):
 	wp ssh db export - --host=$(WP_CLI_HOST) >| $(DATABASE_EXPORT)
 
+production-pull-db:
+	make db-clean WP_CLI_HOST=production $(DATABASE_EXPORT)
+	cat $(DATABASE_EXPORT) | wp ssh db cli --host=vagrant
+	make dev-db-search-replace db-clean dev-plugins
+
+production-push-db: $(DATABASE_EXPORT)
+	cat $(DATABASE_EXPORT) | wp ssh db cli --host=production
+	make production-db-search-replace db-clean
+
+production-db-search-replace:
+	wp ssh search-replace --recurse-objects --network '$(DEV_HOST)' '$(PRODUCTION_HOST)' --host=production
+	wp ssh search-replace --recurse-objects --network '$(LOCAL_HOST)' '$(PRODUCTION_HOST)' --host=production
+
+dev-db-search-replace:
+	wp ssh search-replace --recurse-objects --network '$(PRODUCTION_HOST)' '$(DEV_HOST)' --host=vagrant
+	wp ssh search-replace --recurse-objects --network '$(LOCAL_HOST)' '$(DEV_HOST)' --host=vagrant
+
+db-clean:
+	rm -f $(DATABASE_EXPORT)
+
 # Files -----------------------------------------------------------------------
 
-vm-fetch-files:
+dev-fetch-files:
 	vagrant ssh-config --host default > /tmp/vagrant-ssh-config
 	rsync -r -e 'ssh -F /tmp/vagrant-ssh-config' default:/var/www/wordpress/web/app/uploads/ web/app/uploads/
 	rm -f /tmp/vagrant-ssh-config
+
+production-pull-files:
+	rsync -v -r -e 'ssh -o ForwardAgent=yes -o ProxyCommand="ssh deploy@minasanor.genero.fi nc %h %p 2> /dev/null"' $(PRODUCTION_REMOTE_HOST)/deploy/current/web/app/uploads/ web/app/uploads/
+	vagrant ssh-config --host default >| /tmp/vagrant-ssh-config
+	rsync -v -r --no-perms --no-owner --no-group --verbose -e 'ssh -F /tmp/vagrant-ssh-config' web/app/uploads/ default:/var/www/wordpress/web/app/uploads/
+
+production-push-files: dev-fetch-files
+	rsync -v -r -e 'ssh -o ForwardAgent=yes -o ProxyCommand="ssh deploy@minasanor.genero.fi nc %h %p 2> /dev/null"' web/app/uploads/ $(PRODUCTION_REMOT_HOST)/deploy/current/web/app/uploads/
+
+# Plugins setup ---------------------------------------------------------------
+
+dev-plugins:
+	wp ssh plugin activate debug-bar kint-debugger --host=vagrant
+	wp ssh plugin deactivate autoptimize --host=vagrant
 
 .PHONY: all vm-clean vm-fetch-files
--- config/deploy.rb
+++ config/deploy.rb
@@ -16,12 +16,19 @@ set :log_level, :info
 # Apache users with .htaccess files:
 # it needs to be added to linked_files so it persists across deploys:
 set :linked_files, fetch(:linked_files, []).push('.env')
-set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads', 'web/app/cache')
 
 # We will after the deploy set all uploaded files to have the permissions of the customer
 # So that the quota on the server will be counted towards that user.
 set :file_permissions_paths, ["web"]
 set :file_permissions_users, [fetch(:customer_username)]
+
+set :tail_options,            "-n 100 -f"
+set :rsync_options,           "--recursive --times --compress --human-readable --progress"
+
+# Assets
+set :assets_compile,          "npm run-script build"
+set :assets_output,           %w[web/app/themes/example/dist web/app/themes/example/bower_components]
 
 # Slackistrano (change to true)
 set :slack_run_starting,     -> { false }
@@ -30,58 +37,52 @@ set :slack_run_failed,       -> { false }
 # Add an incoming webhook at https://<team>.slack.com/services/new/incoming-webhook
 # set :slack_webhook, "https://hooks.slack.com/services/XXX/XXX/XXX"
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :service, :nginx, :reload
-    end
-  end
-end
-
-# The above restart task is not run by default
-# Uncomment the following line to run it on deploys if needed
-# after 'deploy:publishing', 'deploy:restart'
-
-namespace :deploy do
-  desc 'Update WordPress template root paths to point to the new release'
-  task :update_option_paths do
-    on roles(:app) do
-      within fetch(:release_path) do
-        if test :wp, :core, 'is-installed'
-          [:stylesheet_root, :template_root].each do |option|
-            # Only change the value if it's an absolute path
-            # i.e. The relative path "/themes" must remain unchanged
-            # Also, the option might not be set, in which case we leave it like that
-            value = capture :wp, :option, :get, option, raise_on_non_zero_exit: false
-            if value != '' && value != '/themes'
-              execute :wp, :option, :set, option, fetch(:release_path).join('web/wp/wp-content/themes')
-            end
-          end
-        end
-      end
-    end
-  end
-end
-
-# The above update_option_paths task is not run by default
-# Note that you need to have WP-CLI installed on your server
-# Uncomment the following line to run it on deploys if needed
-# after 'deploy:publishing', 'deploy:update_option_paths'
-#
-
-after "deploy:publishing", "composer:install"
-
-# After we have put all the files where they should be, we chante ownership from deploy to the customers on all files
+# Task definitions
 namespace :deploy do
   desc 'Change ownership of deployed files'
   task :chown do
     on roles(:app) do
       execute :sudo, "chown -R #{fetch(:customer_username)}:deploy #{release_path}"
-      execute :sudo, "chmod g+w #{release_path}"
+      execute :sudo, "chmod -R g+w #{release_path}"
+    end
+  end
+
+end
+
+namespace :cache do
+  desc 'Flush WP Super Cache'
+  task 'flush-wpcc' do
+    on roles(:app) do
+      within current_path do
+        execute :sudo, :chown, '-R', 'deploy:deploy', 'web/app/cache/supercache'
+        execute :rm, '-rf', 'web/app/cache/supercache/*'
+      end
+    end
+  end
+
+  desc 'Flush Autoptimize Cache'
+  task 'flush-autoptimize' do
+    on roles(:app) do
+      within current_path do
+        execute :sudo, :chown, '-R', 'deploy:deploy', 'web/app/cache/autoptimize'
+        execute :rm, '-rf', 'web/app/cache/autoptimize/*'
+      end
     end
   end
 end
 
-after "composer:install", "deploy:chown"
+# Sanity check
+before "deploy:starting", "deploy:check:pushed"
+before "deploy:starting", "deploy:check:assets"
+before "deploy:starting", "deploy:check:sshagent"
+
+# Install plugins
+before "deploy:updated", "composer:install"
+# Compiled and rsync assets
+after "deploy:updated", "assets:push"
+
+# Change the file owner to the customer
+after "deploy:updated", "deploy:chown"
+# Clear the cache
+after "deploy:published", "cache:flush-autoptimize"
+after "deploy:published", "cache:flush-wpcc"
