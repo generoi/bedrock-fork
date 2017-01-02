48bd75e613166189606259e4171dbf5f00b9c31c
Oskar Schöldström <public@oxy.fi>
2017-01-02 08:08:32 -0500

    switch to minasithil

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,15 +1,15 @@
 set :stage,     :staging
-set :app_url,   'http://<example-project>.web.staging.minasanor.genero.fi'
-set :deploy_to, '/var/www/staging/<example-project>'
+set :app_url,   'http://sage-dev.web.staging.minasithil.genero.fi'
+set :deploy_to, '/var/www/staging/sage-dev'
 
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@minasanor.genero.fi}
-role :web, %w{deploy@minasanor.genero.fi}
-role :db,  %w{deploy@minasanor.genero.fi}
+role :app, %w{deploy@minasithil.genero.fi}
+role :web, %w{deploy@minasithil.genero.fi}
+role :db,  %w{deploy@minasithil.genero.fi}
 
 set :ssh_options, {
   forward_agent: true
